737ba2951456419a03cc9100713c97ebda3d7b85
hi <hi>
2020-09-29 16:50:41 -0400

    added main loop

--- Factor.java
+++ Factor.java
@@ -14,11 +14,14 @@ public class Factor {
         Scanner input = new Scanner(System.in);
         int num = Integer.parseInt(input.nextLine());
 
-        
 
-
-
-
+        for (int i = 0; i < (num/2); i++) {
+            if (num % i == 0) {
+                System.out.println(i + " factor");
+            }
+            
+            System.out.println(i + " not");
+        }
 
         input.close();
     }
