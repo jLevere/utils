b50dc5510cda57ea3223b047b1f765944f37556c
hi <hi>
2020-09-29 16:44:20 -0400

    inital commit

---
+++ Factor.java
@@ -0,0 +1,25 @@
+/**
+ * Prints the factors of a given number
+ * @author jackson (leverett.3@osu.edu)
+ * @version 20200929
+ */
+import java.util.Scanner;
+
+public class Factor {
+    
+
+    public static void main(String[] args) {
+
+        //set up objects
+        Scanner input = new Scanner(System.in);
+        int num = Integer.parseInt(input.nextLine());
+
+        
+
+
+
+
+
+        input.close();
+    }
+}
