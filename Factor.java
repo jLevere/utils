c541ff7248f1a28b3df80e3d211f90c3a9ea3de9
hi <hi>
2020-09-29 17:01:40 -0400

    fixed divby0 and added inital message

--- Factor.java
+++ Factor.java
@@ -12,15 +12,18 @@ public class Factor {
 
         //set up objects
         Scanner input = new Scanner(System.in);
+
+        //get input
+        System.out.print("Enter a number: ");
         int num = Integer.parseInt(input.nextLine());
 
 
-        for (int i = 0; i < (num/2); i++) {
+        for (int i = 1; i < (num/2); i++) {
             if (num % i == 0) {
                 System.out.println(i + " factor");
-            }
-            
-            System.out.println(i + " not");
+            } else {
+                System.out.println(i + " not");
+            }          
         }
 
         input.close();
